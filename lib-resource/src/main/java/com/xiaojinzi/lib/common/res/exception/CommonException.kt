fc1d95637cb325fc2547b3913e367d5de4e6e978
xiaojiniz <347837667@qq.com>
2022-09-28 20:55:30 +0800

    feat: CommonBusinessException 增加获取 StringItem 的方法

--- lib-resource/src/main/java/com/xiaojinzi/lib/common/res/exception/CommonException.kt
+++ lib-resource/src/main/java/com/xiaojinzi/lib/common/res/exception/CommonException.kt
@@ -1,5 +1,8 @@
 package com.xiaojinzi.lib.common.res.exception
 
+import com.xiaojinzi.support.bean.StringItemDto
+import com.xiaojinzi.support.ktx.toStringItemDto
+
 /**
  * 表示业务异常
  */
@@ -7,7 +10,14 @@ open class CommonBusinessException(
     val messageRsd: Int? = null,
     message: String? = null,
     cause: Throwable? = null,
-) : RuntimeException(message, cause)
+) : RuntimeException(message, cause) {
+
+    val messageStringItem: StringItemDto?
+        get() {
+            return messageRsd?.toStringItemDto() ?: message?.toStringItemDto()
+        }
+
+}
 
 /**
  * 表示不用管的异常
